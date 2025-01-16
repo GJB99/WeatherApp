d81c4727f9a277ab11f7d8a6617a82d07280a716
GJB99 <guus@bouwens.nl>
2025-01-16 22:08:04 +0100

    fixed arrow

--- weather-app/src/app/WindDirectionArrow.tsx
+++ weather-app/src/app/WindDirectionArrow.tsx
@@ -6,10 +6,12 @@ interface WindDirectionArrowProps {
 }
 
 export default function WindDirectionArrow({ degrees, className = "" }: WindDirectionArrowProps) {
+  const arrowDirection = (degrees + 180) % 360;
+  
   return (
     <ArrowUpIcon 
       className={`h-4 w-4 inline-block ${className}`} 
-      style={{ transform: `rotate(${degrees}deg)` }}
+      style={{ transform: `rotate(${arrowDirection}deg)` }}
     />
   )
 }
